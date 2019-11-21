8ea153ec95504618aaecc9c295188798a25e7cce
Samuele Tortomasi <samuele.tortomasi@hs-furtwangen.de>
2019-11-21 13:07:30 +0100

    src und pom.xml

---
+++ src/main/java/de/hfu/App.java
@@ -0,0 +1,15 @@
+package de.hfu;
+import java.util.Scanner;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    Scanner scan = new Scanner(System.in);
+	String speicher = scan.nextLine();
+	System.out.println(speicher.toUpperCase());
+    }
+}
